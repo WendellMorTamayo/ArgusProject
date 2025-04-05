f6b345e6a5c3515cf4b3fb864aeba9ed5b7b2c81
WendellMorTamayo <tamayowendell05@gmail.com>
2025-04-05 10:25:33 +0800

    Add LendTokenDetailsBySlot and LendTokenDetailsBySubject records for loan management

---
+++ Models/Entity/LendTokenDetailsBySlot.cs
@@ -0,0 +1,91 @@
+using Argus.Sync.Data.Models.Enums;
+using ArgusProject.Models.Enums;
+
+namespace ArgusProject.Models.Entity;
+
+public record LendTokenDetailsBySlot
+{
+    public string Subject { get; set; }
+    public ulong Slot { get; init; }
+    public string TxHash { get; init; }
+    public ulong TxIndex { get; init; }
+    public string OwnerPkh { get; init; }
+    public string BorrowerPkh { get; set; }
+    public byte[] UtxoRaw { get; init; }
+    public ulong LoanAmount { get; init; }
+    public ulong InterestAmount { get; init; }
+    public ulong UtxoAmount { get; init; }
+    public ulong TokenAmount { get; init; }
+    public string OwnerAddress { get; init; }
+    public string BorrowerAddress { get; set; }
+    public ulong LoanDuration { get; set; }
+    public ulong LoanEndTime { get; set; }
+    public string OutputRefTxHash { get; init; }
+    public uint OutputRefTxIndex { get; init; }
+    public LevvyDatumType DatumType { get; init; }
+    public UtxoStatus UtxoStatus { get; init; } = UtxoStatus.Unspent;
+    public string SpentTxHash { get; init; }
+    public TokenAction ActionType { get; init; }
+    public string? ScriptHash { get; set; } = null;
+
+    public LendTokenDetailsBySlot(
+        string Subject,
+        ulong Slot,
+        string TxHash,
+        ulong TxIndex,
+        string OwnerPkh,
+        string BorrowerPkh,
+        byte[] UtxoRaw,
+        ulong LoanAmount,
+        ulong InterestAmount,
+        ulong UtxoAmount,
+        ulong TokenAmount,
+        string OwnerAddress,
+        string BorrowerAddress,
+        ulong LoanDuration,
+        ulong LoanEndTime,
+        string OutputRefTxHash,
+        uint OutputRefTxIndex,
+        LevvyDatumType DatumType,
+        string SpentTxHash,
+        TokenAction ActionType,
+        UtxoStatus UtxoStatus = UtxoStatus.Unspent,
+        string? ScriptHash = null
+
+    )
+    {
+        this.Subject = Subject;
+        this.Slot = Slot;
+        this.TxHash = TxHash;
+        this.TxIndex = TxIndex;
+        this.OwnerPkh = OwnerPkh;
+        this.BorrowerPkh = BorrowerPkh;
+        this.UtxoRaw = UtxoRaw;
+        this.LoanAmount = LoanAmount;
+        this.InterestAmount = InterestAmount;
+        this.UtxoAmount = UtxoAmount;
+        this.TokenAmount = TokenAmount;
+        this.OwnerAddress = OwnerAddress;
+        this.BorrowerAddress = BorrowerAddress;
+        this.LoanDuration = LoanDuration;
+        this.LoanEndTime = LoanEndTime;
+        this.OutputRefTxHash = OutputRefTxHash;
+        this.OutputRefTxIndex = OutputRefTxIndex;
+        this.DatumType = DatumType;
+        this.SpentTxHash = SpentTxHash;
+        this.ActionType = ActionType;
+        this.UtxoStatus = UtxoStatus;
+        this.ScriptHash = ScriptHash;
+    }
+
+    // public TransactionOutput? Utxo => CborSerializer.Deserialize<TransactionOutput>(UtxoRaw);
+    // public TokenDatum? TokenDatum => Utxo switch
+    // {
+    //     PostAlonzoTransactionOutput babbage => babbage.Datum switch
+    //     {
+    //         InlineDatumOption inlineDatum => CborSerializer.Deserialize<TokenDatum>(inlineDatum.Data.Value),
+    //         _ => null
+    //     },
+    //     _ => null
+    // };
+}
